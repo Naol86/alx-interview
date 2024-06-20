2299499780c298a10a1234a1691a5acf5c82263b
naol86 <naol.kasinet@a2sv.org>
2024-06-20 14:37:23 +0300

    test

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,17 +2,20 @@
 '''A module for working with Pascal's triangle.
 '''
 
+
 def pascal_triangle(n):
-  '''Creates a list of lists of integers representing
-  the Pascal's triangle of a given integer.
-  '''
-  if n <= 0:
-    return []
-  ans = [[1]]
-  for i in range(1, n):
-    ans.append([1])
-    for j in range(len(ans[-2])-1):
-      ans[-1].append(ans[-2][j] + ans[-2][(j+1)%len(ans[-2])])
-    ans[-1].append(1)
-  return ans
-print(pascal_triangle(5))
+    '''Creates a list of lists of integers representing
+    the Pascal's triangle of a given integer.
+    '''
+    triangle = []
+    if type(n) is not int or n <= 0:
+        return triangle
+    for i in range(n):
+        line = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                line.append(1)
+            elif i > 0 and j > 0:
+                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(line)
+    return triangle
