1497216ed31a79956767871bace2e05161acd598
naol86 <naol.kasinet@a2sv.org>
2024-06-20 16:50:27 +0300

    test

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,49 +1,37 @@
 #!/usr/bin/python3
+""" log parsing """
+
 import sys
-import signal
 
-total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
+if __name__ == '__main__':
 
-def print_stats():
-    print(f"File size: {total_size}")
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print(f"{code}: {status_codes[code]}")
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {t: 0 for t in codes}
 
-def signal_handler(sig, frame):
-    print_stats()
-    sys.exit(0)
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(filesize))
+        for t, j in sorted(stats.items()):
+            if j:
+                print("{}: {}".format(t, j))
 
-signal.signal(signal.SIGINT, signal_handler)
-
-try:
-    for line in sys.stdin:
-        parts = line.split()
-        if len(parts) != 10:
-            continue
-        
-        ip, _, _, date, _, request, _, status, size, _ = parts
-        
-        if not request.startswith('"GET /projects/260 HTTP/1.1"'):
-            continue
-        
-        try:
-            status = int(status)
-            size = int(size)
-        except ValueError:
-            continue
-
-        total_size += size
-
-        if status in status_codes:
-            status_codes[status] += 1
-
-        line_count += 1
-
-        if line_count % 10 == 0:
-            print_stats()
-except KeyboardInterrupt:
-    print_stats()
-    sys.exit(0)
+    try:
+        for line in sys.stdin:
+            count += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                filesize += int(data[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
+    except KeyboardInterrupt:
+        print_stats(stats, filesize)
+        raise
